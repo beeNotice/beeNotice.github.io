2e832402a5bd099b2f3f4044eed3e1286b3bf906
fmartin <fabien.martin78@gmail.com>
2016-03-01 10:48:52 +0100

    Work on search

---
+++ js/search.js
@@ -0,0 +1,53 @@
+var postTemplate = _.template(
+	['<div class="article-row">',
+	' <article class="article article-summary full-width">',
+	'   <div class="article-summary-inner">',
+	'      <h2 class="article-title">',
+	'        <a href="<%= post.url %>" class="title"><%= post.title %></a>',
+	'      </h2>',
+	'      <p class="article-excerpt">',
+	'        <%= post.excerpt %>',
+	'      </p>',
+	'    </div>',
+	'  </article>',
+	'</div>'].join(''));
+
+var search = function(posts) {
+
+	var queryParams = (function(queries) {
+		if (queries == "")
+			return {};
+
+		var params = {};
+		for (var i = 0; i < queries.length; ++i) {
+			var param = queries[i].split('=', 2);
+			if (param.length == 1)
+				params[param[0]] = "";
+			else
+				params[param[0]] = decodeURIComponent(param[1].replace(/\+/g, " "));
+		}
+		return params;
+	})(window.location.search.substr(1).split('&'));
+
+	var searched = queryParams['q'];
+	$('#page-title').text('Results for "' + searched + '"');
+	$('#search-form').val(searched);
+
+	var searchRegex = new RegExp(searched, 'i');
+	_(posts)
+	.filter(function (post) {
+		var categoryMatch = _.any(post.categories, function (test) {
+			return searchRegex.test(test)
+		});
+		var tagMatch = _.any(post.tags, function (test) {
+			return searchRegex.test(test)
+		});
+		return categoryMatch
+		|| tagMatch
+		|| post.excerpt.match(searchRegex)
+		|| post.title.match(searchRegex);
+	})
+	.forEach(function(matchingPost) {
+		$('#search-results').append(postTemplate({post: matchingPost}));
+	});
+}
